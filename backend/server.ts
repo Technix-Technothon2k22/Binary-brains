4afa2c44d258c8285d453bb8f0724de61f00989d
Siddhesh Dabholkar <iamsiddhesh22@gmail.com>
2022-05-27 14:34:20 +0530

    db connected

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
+import { MONGO_URI } from "./key";
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(port, () => console.log("app connected succesfully"));
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("connected to mongodb successfully");
+    return app.listen(port, () => console.log("app connected succesfully"));
+  })
+  .catch((err) => console.log(err));
