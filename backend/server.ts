bb2260cbf863a90eda8f1200aa2fcfabe36041a1
Siddhesh Dabholkar <iamsiddhesh22@gmail.com>
2022-05-27 12:56:50 +0530

    reset

--- backend/server.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import mongoose from "mongoose";
-const port = process.env.PORT || 5000;
-
-const app = express();
-
-app.listen(port, () => console.log("app connected succesfully"));
